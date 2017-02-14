fa338b3c13a6ddfebb5b19971942b572ae417061
ushiboy <ushiboy.dev@gmail.com>
2017-02-14 09:55:09 +0900

    add cli

---
+++ bin/mdcat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const mdcat = require('../lib/cli');
+
+mdcat.run(process.argv.slice(2));
--- src/index.js
+++ src/index.js
@@ -7,41 +7,43 @@ import url from 'url';
 import { server as WebSocketServer } from 'websocket';
 import { routes, respond404 } from './route';
 
-const cwd = process.cwd();
-
-const httpServer = http.createServer((req, res) => {
-  const pathname: any = url.parse(req.url).pathname;
-  const route = routes.find(r => r.match(pathname));
-  const env = {
-    cwd,
-    themeCssPath: path.join(__dirname, '../node_modules/github-markdown-css/github-markdown.css')
-  };
-  if (route) {
-    route.handle(req, res, env);
-  } else {
-    respond404(res);
-  }
-});
-
-const wsServer = new WebSocketServer({
-  httpServer,
-  autoAcceptConnections: false
-});
-
-wsServer.on('request', function(request) {
-  const connection = request.accept(null, request.origin);
-  const sendReload = () => {
-    connection.sendUTF('reload');
-  };
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      const markdownFile = message.utf8Data;
-      fs.watchFile(markdownFile, { interval: 500 }, sendReload);
-      connection.on('close', function(reasonCode, description) {
-        fs.unwatchFile(markdownFile, sendReload);
-      });
+export function start(option: any) {
+  const { port, cwd, markdown, themeCssPath } = option;
+  const httpServer = http.createServer((req, res) => {
+    const pathname: any = url.parse(req.url).pathname;
+    const route = routes.find(r => r.match(pathname));
+    const env = {
+      cwd,
+      themeCssPath
+    };
+    if (route) {
+      route.handle(req, res, env);
+    } else {
+      respond404(res);
     }
   });
-});
 
-httpServer.listen(8080);
+  const wsServer = new WebSocketServer({
+    httpServer,
+    autoAcceptConnections: false
+  });
+
+  wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin);
+    const sendReload = () => {
+      connection.sendUTF('reload');
+    };
+    connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+        const markdownFile = message.utf8Data;
+        fs.watchFile(markdownFile, { interval: 500 }, sendReload);
+        connection.on('close', function(reasonCode, description) {
+          fs.unwatchFile(markdownFile, sendReload);
+        });
+      }
+    });
+  });
+
+  httpServer.listen(port);
+  console.log(`listen ${port} port.`);
+}
